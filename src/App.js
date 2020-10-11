1b2be1b82ca5a3b4752e66b2a1986ebee3845da2
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-10-11 17:06:16 +0200

    clarifai API settings & faceRecognition components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 const app = new Clarifai.App({
-  apiKey: '2954123e222d4e28960202e4616bfbc7',
+  apiKey: 'c80497c3d8c84120944582b5eeb5f7ae',
 });
 
 const particlesOptions = {
@@ -28,25 +29,53 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
+      box: {}
     };
-    console.log(app);
+    //console.log(app);
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    //console.log(width, height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  } 
+
+  displayFaceBox = (box) => {
+    console.log(box);
+
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   };
 
   onButtonSubmit = () => {
-    console.log('click');
-    console.log(app);
+    //console.log('click');
+    //console.log(app);
+
+    this.setState({imageUrl: this.state.input});
 
     app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, 'https://samples.clarifai.com/face-det-jpg')
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then((response) => {
-        console.log(response);
+        //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        /*  get all the infos needed from the API  */
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch((error) => {
-        /*console.log(error);*/
+        console.log(error);
       });
   };
 
@@ -58,7 +87,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        {/*<FaceRecognition />*/}
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
