e89776bccf9105f797a4e2cd52616f6b28ca4e30
Julien Orjollet <oj.professionnel@gmail.com>
2021-03-11 18:15:00 +0100

    changes on App.js

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
       }
     });
   };
-  
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
@@ -72,43 +72,43 @@ class App extends Component {
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
     };
-  }; 
+  };
 
   displayFaceBox = (box) => {
     console.log(box);
 
-    this.setState({box: box});
+    this.setState({ box: box });
   };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
     //console.log('click');
     //console.log(app);
 
-    this.setState({imageUrl: this.state.input});
+    this.setState({ imageUrl: this.state.input });
 
-      /*  fetch needs a response (json)   */
-      fetch('https://mysterious-meadow-18725.herokuapp.com/imageurl', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json'},
-        /** send some unique formated json data
-        *    with as object the data  we want to put
-        * here we only need the id to handle the put req 
-        **/
-        body: JSON.stringify({
-          input: this.state.input
-        })
+    /*  fetch needs a response (json)   */
+    fetch('https://mysterious-meadow-18725.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      /** send some unique formated json data
+      *    with as object the data  we want to put
+      * here we only need the id to handle the put req 
+      **/
+      body: JSON.stringify({
+        input: this.state.input
       })
+    })
       .then(response => response.json())
       .then(response => {
-        if(response) {
+        if (response) {
           /*  fetch needs a response (json)   */
           fetch('https://mysterious-meadow-18725.herokuapp.com/image', {
             method: 'put',
-            headers: { 'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             /** send some unique formated json data
             *    with as object the data  we want to put
             * here we only need the id to handle the put req 
@@ -117,11 +117,11 @@ class App extends Component {
               id: this.state.user.id
             })
           })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, { entries: count }));
-          })
-          .catch(console.log);
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            })
+            .catch(console.log);
         }
         //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         /*  get all the infos needed from the API  */
@@ -134,9 +134,9 @@ class App extends Component {
     if (route === 'signout') {
       this.setState(initialState);
     } else if (route === 'home') {
-      this.setState({isSignedIn: true});
-    } 
-    this.setState({route : route});
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
   };
 
 
@@ -147,28 +147,28 @@ class App extends Component {
     return (
       <div className='App'>
         <Particles className='particles' params={particlesOptions} />
-        <Navigation 
+        <Navigation
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange} />
         { route === 'home'
           ? <div>
-              <Logo />
-              <Rank 
-                name={this.state.user.name}
-                entries={this.state.user.entries} />
-              <ImageLinkForm 
-                onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
-            </div>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries} />
+            <ImageLinkForm
+              onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
           : (
-              route === 'signin'    
-              ? <SignIn 
-                  loadUser={this.loadUser} 
-                  onRouteChange={this.onRouteChange}  /> 
-              : <Register 
-                  loadUser={this.loadUser}
-                  onRouteChange={this.onRouteChange}  /> 
-            )
+            route === 'signin'
+              ? <SignIn
+                loadUser={this.loadUser}
+                onRouteChange={this.onRouteChange} />
+              : <Register
+                loadUser={this.loadUser}
+                onRouteChange={this.onRouteChange} />
+          )
         }
       </div>
     );
